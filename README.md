098dc512e433347435e175bfc400b3330283ffe8
EyePulp <eyepulp@eyepulp.net>
2022-12-05 11:36:56 -0600

    day 1 solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2022 Advent of Code
+
+- day 1 - `deno run --allow-read src/day1/main.ts`
---
+++ src/day1/main.ts
@@ -0,0 +1,18 @@
+
+/**
+ * day1.ts
+ * deno run --allow-read main.ts
+ */
+import { sum } from "https://deno.land/x/sum/mod.ts";
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const text = await Deno.readTextFile(__dirname + "data.txt");
+const sums = text.split('\n\n').reduce((a,v)=>{
+    a.push(sum(v.split('\n').map(i=>parseInt(i)).filter(Boolean)));
+    return a;
+},[]).sort().reverse();
+
+const largest = sums[0];        // part 1 answer
+const top_3 = sums.slice(0,3);  // part 2 answer
+
+console.log(largest, sum(top_3));
